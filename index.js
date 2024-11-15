32c9800485dd82aecdff84cbd8577b9461e05046
Luis Corena <luedco2009@gmail.com>
2024-11-15 08:19:03 -0500

    fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('¡Hola, mundo!');
+  res.send('¡Hola, mundo2!');
 });
 
 app.listen(PORT, () => {
