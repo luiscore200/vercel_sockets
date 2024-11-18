b540ce70607ad2c6affc24964c3b14b4d88c80d8
Luis Corena <luedco2009@gmail.com>
2024-11-18 19:00:31 +0000

    fix

--- routes/api.js
+++ routes/api.js
@@ -5,12 +5,12 @@ const router = express.Router();
 
 // Ruta para iniciar sesión
 router.post('/start-session', WpController.startSession);
-router.get('/start-session', WpController.startSession);
+router.get('/start-session-get', WpController.startSession);
 
 // Ruta para enviar mensaje
 router.post('/send-message', WpController.sendMessage);
-router.get('/send-message', WpController.sendMessage);
+router.get('/send-message-get', WpController.sendMessage);
 
 router.post('/get-contacts', WpController.getContact);
-router.get('/get-contacts', WpController.getContact);
+router.get('/get-contacts-get', WpController.getContact);
 module.exports = router;
